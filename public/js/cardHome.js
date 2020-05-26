5ae7f58d6cee0379957bd5688b1ef1a0a4e3797f
tp <antoine.guillotin@cpe.fr>
2020-05-26 16:19:20 +0200

    cardHome ok

--- public/js/cardHome.js
+++ public/js/cardHome.js
@@ -1,15 +1,47 @@
+
+
+
+var userConnect = ""
+
 $(document ).ready(function(){
+    
+//    var xml =new XMLHttpRequest();
+//    xml.onLoad=function(response){
+//    	console.log("load");
+//    	console.log(response.reponspeText);
+//    }
+//    xml.open("GET","user");
+//    xml.send();
+    $.ajax("user",{success:function(user){
+    	userConnect = user.name;
+        $("#userNameId").html(user.name);
+        $("#money span").html("42");
+
+    },error:function(a){
+        console.log("error",a);
+        $("#userNameId").html(`<a href="formSample.html">Login</a>`);
+        $("#money").hide();
+    }})
+
+
+
     $("#playButtonId").click(function(){
         alert("Play button clicked ");
-        //TO DO
+        
     });    
     $("#buyButtonId").click(function(){
-        alert("Buy button clicked ");
-        //TO DO
+    	if(userConnect){
+    		window.location.href="searchCard.html";    		
+    	}else{
+    		window.location.href="formSample.html";
+    	}
     });    
     $("#sellButtonId").click(function(){
-        alert("Sell button clicked ");
-        //TO DO
+    	if(userConnect){
+	    	window.location.href="cardList.html";
+	    }else{
+			window.location.href="formSample.html";
+		}
     });    
 });
 
